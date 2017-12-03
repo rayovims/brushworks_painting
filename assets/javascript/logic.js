8f9765e007531f3fcb55d2df32969fb20eef58bd
Raymundo Villanueva <Raymundo Ojeda>
2017-12-02 23:23:15 -0500

    final

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,38 @@
-
-$(function scroll(){
-  $(window).scroll(function() {
-    var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
-    console.log(wintop);
-    var totalScroll = (wintop/(docheight-winheight))*100;
-    console.log("total scroll" + totalScroll);
-    $(".progressBar").css("width",totalScroll+"%");
-    $(".roller").css("left",totalScroll+"%");
-  });
+$(function scroll1() {
+    $(window).scroll(function () {
+        var wintop = $(window).scrollTop(),
+            docheight = $("#main").height(),
+            winheight = $(window).height();
+           //console.log(wintop);
+        var totalScroll = (wintop / winheight) * 100;
+        //    console.log("total scroll" + totalScroll);
+        $("#progressBar1").css("width", totalScroll + "%");
+        $("#roller1").css("left", totalScroll - 2 + "%");
+    });
+});
+$(function scroll2() {
+    $(window).scroll(function () {
+        var wintop = $(window).scrollTop(),
+            docheight = $("#main").height(),
+            winheight = $(window).height();
+        console.log(wintop);
+        console.log(winheight);
+        var totalScroll = ((wintop-950) / winheight) * 100;
+        console.log("total scroll" + totalScroll);
+        $("#progressBar2").css("width", totalScroll + "%");
+        $("#roller2").css("left", totalScroll - 2 + "%");
+    });
+});
+$(function scroll3() {
+    $(window).scroll(function () {
+        var wintop = $(window).scrollTop(),
+            docheight = $("#main").height(),
+            winheight = $(window).height();
+        console.log(wintop);
+        console.log(winheight);
+        var totalScroll = ((wintop-1900) / winheight) * 100;
+        console.log("total scroll" + totalScroll);
+        $("#progressBar3").css("width", totalScroll + "%");
+        $("#roller3").css("left", totalScroll - 2 + "%");
+    });
 });
