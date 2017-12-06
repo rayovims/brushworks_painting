866499156c383bd7e29c674b6e8661512a4f9d45
Raymundo Villanueva <Raymundo Ojeda>
2017-12-05 22:12:11 -0500

    Final

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,36 +1,34 @@
 $(function scroll1() {
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         var wintop = $(window).scrollTop(),
             docheight = $("#main").height(),
             winheight = $(window).height();
-           //console.log(wintop);
         var totalScroll = (wintop / winheight) * 100;
-        //    console.log("total scroll" + totalScroll);
         $("#progressBar1").css("width", totalScroll + "%");
         $("#roller1").css("left", totalScroll - 2 + "%");
     });
 });
 $(function scroll2() {
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         var wintop = $(window).scrollTop(),
             docheight = $("#main").height(),
             winheight = $(window).height();
         console.log(wintop);
         console.log(winheight);
-        var totalScroll = ((wintop-950) / winheight) * 100;
+        var totalScroll = ((wintop - 950) / winheight) * 100;
         console.log("total scroll" + totalScroll);
         $("#progressBar2").css("width", totalScroll + "%");
         $("#roller2").css("left", totalScroll - 2 + "%");
     });
 });
 $(function scroll3() {
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         var wintop = $(window).scrollTop(),
             docheight = $("#main").height(),
             winheight = $(window).height();
         console.log(wintop);
         console.log(winheight);
-        var totalScroll = ((wintop-1900) / winheight) * 100;
+        var totalScroll = ((wintop - 1900) / winheight) * 100;
         console.log("total scroll" + totalScroll);
         $("#progressBar3").css("width", totalScroll + "%");
         $("#roller3").css("left", totalScroll - 2 + "%");
